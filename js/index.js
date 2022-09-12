1d1604780eb0905d440c639bc8e5ab62cc6bf6ed
MarianSoltermann <mariansoltermann@gmail.com>
2022-09-12 19:32:25 -0300

    calculamos el total del carrito

--- js/index.js
+++ js/index.js
@@ -1,20 +1,33 @@
-//FOR
-/*
-for(let alumno = 1; alumno <= 7; alumno++){
-  let nombre = prompt("Ingrese su nombre");
-  let mensaje = `Alumno #${alumno} Nombre: ${nombre}`;
-  alert(mensaje);
+
+let totalPedido = 0;
+let precioUnitario = 0;
+let contador = 0;
+
+alert(`Vamos a sumar los precios de los productos que agregaste al carrito y te mostraremos el total`);
+
+function ingresarPrecio() {
+  precioUnitario = Number(prompt(`Ingrese el precio del producto` + (contador + 1) + `:`));
+
+while (precioUnitario > 0) {
+  totalPedido = calcularTotal(precioUnitario);
+  contador++;
+  precioUnitario = Number(prompt(`Ingrese el precio del producto` + (contador + 1) + `:`));
 }
-*/
-
-//WHILE
-
-let entrada = prompt("Ingrese su contraseña");
-
-while(entrada != "qwerty"){
-  alert("Contraseña incorrecta");
-
-  entrada = prompt("Ingrese su contraseña");
+if (precioUnitario <= 0 || isNaN(precioUnitario)) {
+  console.log(`Ha introducido un número no válido`);
+}
 }
 
-alert("Bienvenido");
+function calcularTotal(precioUnitario) {
+  return totalPedido + precioUnitario;
+}
+
+function mostrarTotal() {
+  console.log(`Cantidad de productos: ` + contador);
+  console.log(`El precio total es $ ` + totalPedido);
+}
+
+
+ingresarPrecio();
+calcularTotal();
+mostrarTotal();
